187e8d3a5a5bfb6b425c1bf93f900def102fa0a3
Ricky <ricky85ps@users.noreply.github.com>
2025-05-09 15:06:22 +0200

    initial repairing docsrs

--- build.rs
+++ build.rs
@@ -13,7 +13,7 @@ mod uber_h3_from_scratch {
         hasher.update(&file_content);
         let checksum = hasher.finalize();
         let sha512 = hex::decode(sha512).expect("Checksum not valid HEX!");
-        if &checksum[..] != &sha512[..] {
+        if checksum[..] != sha512[..] {
             panic!(
                 "Checksum for {} invalid {:?} != {:?}",
                 dest,
@@ -77,7 +77,7 @@ mod uber_h3_from_scratch {
         let src_dir = out_dir.join("uber");
 
         let info = from_toml("Cargo.toml");
-        let targz_name = Path::new(&info.targz_url).iter().last().unwrap();
+        let targz_name = Path::new(&info.targz_url).iter().next_back().unwrap();
 
         std::fs::create_dir_all(&src_dir)?;
         let dwnld_dest = out_dir.join(targz_name);
@@ -105,6 +105,9 @@ fn from_env() -> std::path::PathBuf {
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    #[cfg(docsrs)]
+    return Ok(());
+
     #[cfg(not(feature = "uber_h3_from_scratch"))]
     let h3_inst_prefix = from_env();
 
